a66c29e85baffe770b01a38643164b72b81a847d
nadya <f.i.e.s.t.a@list.ru>
2018-11-20 21:06:56 +0200

    The project has been started!

---
+++ seabattle/Core/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Enums;
+
+namespace Core
+{
+
+
+    public class Cell
+    {
+        public CellState State
+        {
+            get; protected set;
+        }
+
+        public Cell()
+        {
+            this.State = CellState.Closed;
+        }
+    }
+}
---
+++ seabattle/Core/Ship.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Enums;
+
+namespace Core
+{
+    class Ship
+    {
+        public int Size
+        {
+            get; protected set;
+        }
+        public ShipState State
+        {
+            get; protected set;
+        }
+
+        private int Injuries = 0;
+
+        public Ship(int size)
+        {
+            this.Size = size;
+            this.State = ShipState.Unbroken;
+        }
+
+        public void Injure()
+        {
+            if (State == ShipState.Broken) return;
+            this.Injuries++;
+            if (Injuries < Size) this.State = ShipState.Injured;
+            else this.State = ShipState.Broken;
+        }
+
+    }
+}
